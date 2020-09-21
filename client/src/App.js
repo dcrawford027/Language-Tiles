d54945ba126c6adcaf2c5446482ac948736b7051
Dustin Crawford <dustin.crawford.2@gmail.com>
2020-09-21 16:52:56 -0400

    added front end wireframe

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css';
+import { Router } from '@reach/router';
+import Home from './views/Home';
 
 function App() {
   return (
-    <div className="App">
-      
+    <div className="container">
+      <Router>
+        <Home path="/"/>
+      </Router>
     </div>
   );
 }
