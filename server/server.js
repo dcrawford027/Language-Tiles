0acab11254d36c90cb8e6e37db2c01535abede62
Dustin Crawford <dustin.crawford.2@gmail.com>
2020-09-21 16:59:15 -0400

    added some front end

--- server/server.js
+++ server/server.js
@@ -1,14 +1,5 @@
 const express = require('express');
 const cors = require('cors');
-<<<<<<< HEAD
-
-const app = express();
-app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-app.listen(8000, () => console.log('Listening on port 8000'));
-=======
 const sockets = require('socket.io');
 const Message = require('./server/models/message.model');
 
@@ -23,10 +14,9 @@ const server = app.listen(PORT, () => console.log(`Listening on port: ${PORT}`))
 
 const io = sockets(server);
 io.on("connection", async function(socket) {
-  socket.emit("firstConnect", await Message.find({}));
+    socket.emit("firstConnect", await Message.find({}));
 
-  socket.on('createMessage', async data => {
+    socket.on('createMessage', async data => {
     socket.broadcast.emit('newMessage', await Message.create(data));
-  });
+    });
 });
->>>>>>> c305cde22fef0f176d37345c9882ad4d5d5b2c8f
